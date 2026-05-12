eb9c5fc4877c9e5bfbda3d48c629b937906ecebf
Jared Hammett <116196579+Jhammett03@users.noreply.github.com>
2026-05-11 18:29:44 -0700

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+        sset = {}
+        tset = {}
+
+        for i in range(len(s)):
+            sset[s[i]] = sset.get(s[i], 0) + 1
+            tset[t[i]] = tset.get(t[i], 0) + 1
+        
+        return sset == tset
